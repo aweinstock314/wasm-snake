d787eb189e22f6422841501e105d9ff627018f70
Avi Weinstock <aweinstock314@gmail.com>
2020-04-07 12:56:12 -0400

    Avoid buffer overflow when deserializing the rng.

--- src/common/serializable_chacha.rs
+++ src/common/serializable_chacha.rs
@@ -7,24 +7,27 @@ pub struct SerializableChaCha20 {
     rng: rand_chacha::ChaCha20Rng,
 }
 #[derive(Serialize, Deserialize)]
-struct SerializableChaCha20Wrapper(Vec<u8>);
+struct SerializableChaCha20Wrapper<'a>(&'a [u8]);
 
 impl Serialize for SerializableChaCha20 {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        let mut innards = SerializableChaCha20Wrapper(vec![]);
-        innards.0.extend_from_slice(unsafe { slice::from_raw_parts(&self.rng as *const _ as *const u8, mem::size_of_val(&self.rng)) });
-        innards.serialize(serializer)
+        let mut innards = vec![];
+        innards.extend_from_slice(unsafe { slice::from_raw_parts(&self.rng as *const _ as *const u8, mem::size_of_val(&self.rng)) });
+        SerializableChaCha20Wrapper(&innards).serialize(serializer)
     }
 }
 
 impl<'de> Deserialize<'de> for SerializableChaCha20 {
     fn deserialize<D>(deserializer: D) -> Result<SerializableChaCha20, D::Error> where D: Deserializer<'de> {
         let innards = SerializableChaCha20Wrapper::deserialize(deserializer)?;
+        if innards.0.len() != mem::size_of::<SerializableChaCha20>() {
+            return Err(serde::de::Error::invalid_length(innards.0.len(), &"mem::size_of::<SerializableChaCha20>()"));
+        }
         unsafe {
             let mut ret: SerializableChaCha20 = mem::zeroed();
             let mut out: *mut u8 = &mut ret as *mut _ as *mut u8;
-            for b in innards.0.into_iter() {
-                ptr::write(out, b);
+            for b in innards.0.iter() {
+                ptr::write(out, *b);
                 out = out.offset(1);
             }
             Ok(ret)
